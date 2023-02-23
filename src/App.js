179f8f4e2d541abf71577b061906274d4a5d6aed
Nikhil Vikraman <nikhil.v@neoito.com>
2023-02-23 08:53:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Uielement11 from './uielement11'
+export default function App() {
+  return (
+    <div>
+      <Uielement11/>
+    </div>
+  )
+}
---
+++ src/uielement11.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Uielement11 = () => {
+  return (
+   <p>Hello from uielement11 </p>
+  )
+}
+export default Uielement11
